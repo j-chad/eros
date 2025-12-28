565164feec0ca9ed4f4b99b9c17096f517fc06de
Jackson <contact@jacksonc.dev>
2025-12-28 16:35:25 +1300

    fix registration_code field name

--- backend/internal/models/registration_code.go
+++ backend/internal/models/registration_code.go
@@ -4,5 +4,9 @@ import "time"
 
 type RegistrationCode struct {
 	Code      string    `json:"code"`
-	ExpiresAt time.Time `json:"time"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
+
+func (r RegistrationCode) IsExpired() bool {
+	return r.ExpiresAt.Before(time.Now())
 }
