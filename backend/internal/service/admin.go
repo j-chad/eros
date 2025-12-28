6de81980d2da54d4d91ed9171fbebe739a8936f2
Jackson <contact@jacksonc.dev>
2025-12-28 14:04:53 +1300

    add timeouts

--- backend/internal/service/admin.go
+++ backend/internal/service/admin.go
@@ -5,3 +5,7 @@ import "backend/internal/repository"
 type AdminService struct {
 	repo repository.Repository
 }
+
+func NewAdminService(repo repository.Repository) *AdminService {
+	return &AdminService{repo: repo}
+}
