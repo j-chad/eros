9fb764dd9b21adffc22410f890bd1c7d21f4f3af
Jackson <contact@jacksonc.dev>
2025-12-30 10:51:10 +1300

    add favour endpoints

--- backend/internal/service/favour.go
+++ backend/internal/service/favour.go
@@ -18,6 +18,18 @@ func (s *FavourService) ListFavourChoices(ctx context.Context) ([]models.FavourC
 	return s.repo.ListFavourChoices(ctx)
 }
 
-func (s *FavourService) GetFavourCount(ctx context.Context) (int, error) {
+func (s *FavourService) GetFavourCount(ctx context.Context) (models.FavourCount, error) {
 	return s.repo.GetFavourCount(ctx)
+}
+
+func (s *FavourService) ListFavourRequests(ctx context.Context) ([]models.FavourRequest, error) {
+	return s.repo.ListFavourRequests(ctx)
+}
+
+func (s *FavourService) RequestFavour(ctx context.Context, request *models.FavourRequest) error {
+	return s.repo.CreateFavourRequest(ctx, request)
+}
+
+func (s *FavourService) DeleteFavourRequest(ctx context.Context, requestID string) error {
+	return s.repo.DeleteFavourRequest(ctx, requestID)
 }
