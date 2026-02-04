1fd64a25529c6eec83261567d6f2be32631867dd
Jackson Chadfield <contact@jacksonc.dev>
2026-02-05 09:00:04 +1300

    add get graph function

--- backend/internal/service/graph.go
+++ backend/internal/service/graph.go
@@ -1,6 +1,11 @@
 package service
 
-import "backend/internal/repository"
+import (
+	"backend/internal/models"
+	"backend/internal/repository"
+	"context"
+	"time"
+)
 
 type GraphService struct {
 	repo repository.Repository
@@ -8,4 +13,24 @@ type GraphService struct {
 
 func NewGraphService(repo repository.Repository) *GraphService {
 	return &GraphService{repo: repo}
+}
+
+func (s *GraphService) GetGraph(ctx context.Context, graphID string) (*models.Graph, error) {
+	graph, err := s.repo.GetGraph(ctx, graphID)
+	if err != nil {
+		return nil, err
+	}
+
+	currentTime := time.Now()
+	if graph.StartingAt.After(currentTime) {
+		return nil, nil
+	}
+
+	// TODO: shake graph down to only include available nodes and edges
+	// shakeGraph(graph)
+
+	return graph, nil
+}
+
+func shakeGraph(graph *models.Graph) {
 }
