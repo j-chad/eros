ebb1a1f2c42e5db63325c8e6c28c2e77a8ece2e3
Jackson Chadfield <contact@jacksonc.dev>
2026-02-05 08:42:25 +1300

    change client port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+	server: {
+		port: 5174 // Prevent service worker conflicts with the default Vite port (5173)
+	},
 	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
 	define: { 'process.env.NODE_ENV': '"production"' }
 });
