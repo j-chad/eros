f266bc0b88756d635d3d02d6dfe8d1e5c1151249
Jackson Chadfield <contact@jacksonc.dev>
2026-02-16 08:06:05 +1300

    immediate sw install

--- client/src/service-worker.ts
+++ client/src/service-worker.ts
@@ -1,5 +1,15 @@
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+/// <reference types="@sveltejs/kit" />
+/// <reference types="../.svelte-kit/ambient.d.ts" />
+
+declare const self: ServiceWorkerGlobalScope;
+
 import { build, files, prerendered, version } from '$service-worker'
-import { precacheAndRoute, type PrecacheEntry } from 'workbox-precaching'
+import {cleanupOutdatedCaches, precacheAndRoute, type PrecacheEntry} from 'workbox-precaching'
+
+cleanupOutdatedCaches();
 
 const precache_list: PrecacheEntry[] = [...build, ...files, ...prerendered].map((s) => ({
 	url: s,
@@ -7,3 +17,13 @@ const precache_list: PrecacheEntry[] = [...build, ...files, ...prerendered].map(
 }))
 
 precacheAndRoute(precache_list)
+
+self.addEventListener('install', (event) => {
+	self.skipWaiting().catch(() => console.error('Failed to skip waiting during service worker installation'));
+});
+
+self.addEventListener('activate', (event) => {
+	event.waitUntil((async () => {
+		await self.clients.claim().catch(() => console.error('Failed to claim clients during service worker activation'));
+	})());
+});
