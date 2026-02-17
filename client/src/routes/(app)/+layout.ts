301896ccf7fa92ba5af4eea8e96cfdf3f7244f6b
Jackson Chadfield <contact@jacksonc.dev>
2026-02-18 09:27:14 +1300

    add test route

--- client/src/routes/(app)/+layout.ts
+++ client/src/routes/(app)/+layout.ts
@@ -1,13 +1,18 @@
 import { redirect } from '@sveltejs/kit';
 import { browser } from '$app/environment';
-import {isAuthenticated} from '$lib/services/session';
+import {authToken, isAuthenticated} from '$lib/api/auth';
+import {get} from "svelte/store";
 
 export const ssr = false;
 
 export async function load({ url }) {
 	if (!browser) return;
 
-	if (!await isAuthenticated()) {
+	const authed = get(isAuthenticated);
+	const token = get(authToken)
+	console.log('isAuthenticated', authed, 'token', token);
+
+	if (!get(isAuthenticated)) {
 		const returnTo = encodeURIComponent(url.pathname + url.search);
 		throw redirect(307, `/login?returnTo=${returnTo}`);
 	}
--- client/src/routes/+layout.ts
+++ client/src/routes/+layout.ts
@@ -1,5 +1,6 @@
-import {initAuth, setToken} from "$lib/api/auth";
+import {initAuth} from "$lib/api/auth";
 
 export async function load() {
 	await initAuth();
+	console.log('Auth initialized');
 }
