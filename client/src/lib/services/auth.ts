83c0340de74cda4f759de5ab76441594f9591173
Jackson Chadfield <contact@jacksonc.dev>
2026-02-20 19:03:18 +1300

    start cleaning up auth

--- client/src/lib/services/auth.ts
+++ client/src/lib/services/auth.ts
@@ -1,7 +1,11 @@
 import { login as loginRequest } from "$lib/repositories/auth";
 import {setToken} from "$lib/api/auth";
+import { invalidate } from "$app/navigation";
+
+export const AUTH_DEPENDENCY = 'auth:session';
 
 export async function login(registration_code: string, device_info: string): Promise<void> {
 	const token = await loginRequest(registration_code, device_info);
-	return setToken(token)
+	await setToken(token);
+	await invalidate(AUTH_DEPENDENCY)
 }
