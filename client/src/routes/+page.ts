d91978ecccfb0f62ce27d551e561ef3f184195cb
Jackson Chadfield <contact@jacksonc.dev>
2026-03-16 07:39:46 +1300

    tidy up the error page

--- client/src/routes/+page.ts
+++ client/src/routes/+page.ts
@@ -1,5 +1,9 @@
-import {redirect} from "@sveltejs/kit";
+import {error, redirect} from "@sveltejs/kit";
 
 export async function load() {
-	return redirect(307, '/app');
+	throw error(401, {
+		message: 'Unauthorized: No authentication token found',
+		body: null,
+	});
+	// return redirect(307, '/app');
 }
