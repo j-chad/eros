fe83849cb052bb08c2174a913569cc1b30376995
Jackson Chadfield <contact@jacksonc.dev>
2026-03-17 06:35:36 +1300

    add https mode

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,31 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv, type PluginOption} from 'vite';
 import tailwindcss from "@tailwindcss/vite";
+import fs from 'fs'
 
-export default defineConfig({
-	server: {
-		port: 5174 // Prevent service worker conflicts with the default Vite port (5173)
-	},
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
-	define: { 'process.env.NODE_ENV': '"production"' }
+export default defineConfig(({mode}) => {
+	const env = loadEnv(mode, process.cwd(), '')
+
+	const plugins: PluginOption[] = [
+		tailwindcss(),
+		sveltekit()
+	]
+
+	if (mode === 'development') {
+		plugins.push(devtoolsJson())
+	}
+
+	return ({
+		server: {
+			port: 5174, // Prevent service worker conflicts with the default Vite port (5173)
+			allowedHosts: env.ALLOWED_HOSTS ? env.ALLOWED_HOSTS.split(',') : undefined,
+			https: env.HTTPS ? {
+				key: fs.readFileSync('.cert/key.pem'),
+				cert: fs.readFileSync('.cert/cert.pem'),
+			} : undefined
+		},
+		plugins,
+		define: {'process.env.NODE_ENV': JSON.stringify(mode)},
+	});
 });
