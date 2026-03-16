e58d1b35b78297761d183389919f7c084ca18f6a
Jackson Chadfield <contact@jacksonc.dev>
2026-03-17 08:55:05 +1300

    manually register service worker

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+	import { dev } from '$app/environment'
+	import { PUBLIC_SERVICE_WORKER } from '$env/static/public';
+
 	import "../app.css";
 
 	let { children } = $props();
+
+	if (PUBLIC_SERVICE_WORKER === 'true' && 'serviceWorker' in navigator) {
+		addEventListener('load', function () {
+			navigator.serviceWorker.register('./path/to/service-worker.js', {
+				type: dev ? 'module' : 'classic'
+			});
+		});
+	}
 </script>
 
 {@render children()}
