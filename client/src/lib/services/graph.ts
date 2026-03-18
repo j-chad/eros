02f26e287c835e2fb75107246f07847ede70735d
Jackson Chadfield <contact@jacksonc.dev>
2026-03-19 06:55:08 +1300

    throw error if not 503

--- client/src/lib/services/graph.ts
+++ client/src/lib/services/graph.ts
@@ -1,6 +1,7 @@
-import { fetchGraphs } from '$lib/api/graph.api';
-import { getAllGraphs, putGraphs } from '$lib/db/stores/graph';
-import type { GraphSummary } from '$lib/types/graph';
+import {fetchGraphs} from '$lib/api/graph.api';
+import {getAllGraphs, putGraphs} from '$lib/db/stores/graph';
+import type {GraphSummary} from '$lib/types/graph';
+import {isHttpError} from "@sveltejs/kit";
 
 export async function listGraphs(): Promise<GraphSummary[]> {
 	if (navigator.onLine) {
@@ -8,6 +9,10 @@ export async function listGraphs(): Promise<GraphSummary[]> {
 			const graphs = await fetchGraphs();
 			await putGraphs(graphs);
 		} catch (e) {
+			if (isHttpError(e) && e.status !== 503) {
+				throw e;
+			}
+
 			console.warn('Failed to fetch graphs from server, falling back to cache', e);
 		}
 	}
